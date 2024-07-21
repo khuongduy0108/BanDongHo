1ecaa26dabede24d0fd7fc85c378fd469b349f34
Khương Duy <88834681+khuongduy0108@users.noreply.github.com>
2024-07-21 13:32:04 +0700

    Add files via upload

---
+++ Response/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace BanDongHoSolution.Response
+{
+    public class BaseResponse<T>
+    {
+        public bool _success { get; set; }
+        public string _Message { get; set; }
+        public T _Data { get; set; }
+
+        public BaseResponse(bool success, string message, T data)
+        {
+            _success = success;
+            _Message = message;
+            _Data = data;
+        }
+    }
+}
---
+++ Response/PaginationResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace BanDongHoSolution.Response
+{
+    public class PaginationResponse<T>:BaseResponse<T>
+    {
+        public int _totalPages { get; set; }
+        public PaginationResponse(bool success, string message, T data,int totalPages) : base(success, message, data)
+        {
+            _totalPages = totalPages;
+        }
+    }
+}
